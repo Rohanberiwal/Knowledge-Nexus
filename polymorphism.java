61ff591bdf6050df703f7514c6579a5ac56e2f54
ROHAN___BERIWAL <119040957+Rohanberiwal@users.noreply.github.com>
2023-08-09 15:59:17 +0530

    Create polymorphism.java
    polymorphism theory

---
+++ polymorphism.java
@@ -0,0 +1,31 @@
+Polymorphism Property 2 
+Polymorphism is one of the most important property of the oops  .
+The word "poly" means many and the word morphism mean  many ways to represent .
+The word mean many ways to represent  . 
+
+Lmaguage which does not support polyumorphism is reffred to as not a oops langauge . 
+Those are called as the object pases langauges (ops) .
+
+Polymorhpism always occueres at the time of inheritance . 
+There are differernt type of polymorphism .  
+one is the runtime 
+other is the complie time polymorphism . 
+
+Compile time or Static polymorphism -> this is done by method overloading  .
+method ovberloading -> when a class have a multiple function with same name but the name , and the return type can be very different .  
+The best example of this is the mutiple constructors / multiple functions .  
+example -> A a = new A() ;
+	A a = new A(_ some parameter) ;
+this is called the complie time polymorphiasm under the sub branch of method ovberloading . 
+The complier decides which contructor to call at the complie time that is why called the compile time polymorhpism . '
+
+During the method overloading if the nane is samne thern the return type , number of input should be different . 
+order of the type can also be different  ,
+example  -> string a  , int b ;
+example 2  - int a  , string b   ;
+this above alawys works .  The complie time is also called the static polymorphsim  . 
+When there is a double parameter need to be passed and the user passes a int then there is a auto casting of the double to int and the output is printed this is called as the casting  . this happend in flaot and int conversion  . 
+
+Runtime or dynamic polymorhpism - >
+The is acheived by method overriding  .Method overriding is important in this .
+Considerinng a parent class with the parameter , name of the attributes and all the attribures are same even the name is same but body is different then this helps in the implementation of runtime - polymorphiism  .
