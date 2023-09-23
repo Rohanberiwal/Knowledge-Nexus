306a4c9a8475f1df858ab8ab5070d8e9b4d22a82
ROHAN___BERIWAL <119040957+Rohanberiwal@users.noreply.github.com>
2023-09-23 13:55:12 +0530

    Create Generic.java

---
+++ Generic.java
@@ -0,0 +1,7 @@
+Java object oriented programming 
+Abstract class help the subclass define the attributes themselves  .If the method in a class is asbstract then the class is also abstratct  .
+The superclass gives a general from and no delcleartion about the attributes . 
+When the superclass has no body then using the method overiding the subclass can make the attribtues conrete .
+@overrride is an important annotation here . Abstract class are inheritedn but not used directly . 
+Generics 
+This is use to create curstom data structures  , these are use to makes the own linkedlist etc  .
